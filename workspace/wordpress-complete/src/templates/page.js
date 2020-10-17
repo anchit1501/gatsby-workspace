929ee60547a63942d3b9560f14b17fd5a4a61759
anchit1501 <1501.anchit@gmail.com>
2020-10-17 15:15:40 +0530

    fluid image css added in wordpress-complete

--- workspace/wordpress-complete/src/templates/page.js
+++ workspace/wordpress-complete/src/templates/page.js
@@ -1,8 +1,8 @@
 import React from "react"
-
 import Layout from '../components/layout'
 import SEO from "../components/seo"
 import Img from 'gatsby-image'
+import '../assets/scss/index.css'
 export default ({ pageContext }) => {
 
   const page = pageContext.page
@@ -10,10 +10,9 @@ export default ({ pageContext }) => {
   return (
     <Layout>
       <SEO title={page.title} />
-
       <h1>{page.title}</h1>
       <Img
-        className=""
+        className="image-container"
         fluid={page.featuredImage.node.localFile.childImageSharp.fluid}
         alt=""
       />
